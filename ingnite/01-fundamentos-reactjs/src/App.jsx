4eb2264d99756afc74de2b012337cb1eaf7cc8a0
Adriano Bispo <drianvini10@gmail.com>
2025-03-11 14:15:41 -0300

    Iniciando com React - Propriedades

--- ingnite/01-fundamentos-reactjs/src/App.jsx
+++ ingnite/01-fundamentos-reactjs/src/App.jsx
@@ -2,6 +2,16 @@ import { Post } from "./Post";
 
 export function App() {
   return (
-    <Post/>
+    <div>
+      <Post 
+        author="Diego Fernandes"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla rerum rem quo, vero porro deserunt, obcaecati modi, esse sunt aperiam eum. A fugiat iusto itaque eum modi eius nihil aliquam?"
+      />
+
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal!"
+      />
+    </div>
   )
 }
