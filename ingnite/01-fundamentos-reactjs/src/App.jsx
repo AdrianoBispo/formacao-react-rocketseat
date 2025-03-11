bbe23f55504e955a4791a4ffaa1cec927fa2f130
Adriano Bispo <drianvini10@gmail.com>
2025-03-11 15:34:39 -0300

    Estrutura da aplicação - CSS Modules

--- ingnite/01-fundamentos-reactjs/src/App.jsx
+++ ingnite/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="Diego Fernandes"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla rerum rem quo, vero porro deserunt, obcaecati modi, esse sunt aperiam eum. A fugiat iusto itaque eum modi eius nihil aliquam?"
