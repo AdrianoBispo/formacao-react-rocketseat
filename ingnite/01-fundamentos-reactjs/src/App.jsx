5ea13c3af65f5b2e2054c29f155a4027dbb60faf
Adriano Bispo <drianvini10@gmail.com>
2025-03-21 15:25:14 -0300

    Estrutura da aplicação - Componente: Sidebar

--- ingnite/01-fundamentos-reactjs/src/App.jsx
+++ ingnite/01-fundamentos-reactjs/src/App.jsx
@@ -1,22 +1,31 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from './App.module.css';
+
 import './global.css';
+import {Sidebar} from './components/Sidebar.jsx';
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post 
-        author="Diego Fernandes"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla rerum rem quo, vero porro deserunt, obcaecati modi, esse sunt aperiam eum. A fugiat iusto itaque eum modi eius nihil aliquam?"
-      />
+      <div className="{styles.wrapper">
+        <Sidebar />
 
-      <Post
-        author="Gabriel Buzzi"
-        content="Um novo post muito legal!"
-      />
+        <main>
+          <Post 
+            author="Diego Fernandes"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla rerum rem quo, vero porro deserunt, obcaecati modi, esse sunt aperiam eum. A fugiat iusto itaque eum modi eius nihil aliquam?"
+          />
+
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
