c61b3045de58eb85e7d62c7caf25b3891d728ee9
Adriano Bispo <drianvini10@gmail.com>
2025-03-21 16:46:37 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- ingnite/01-fundamentos-reactjs/src/App.jsx
+++ ingnite/01-fundamentos-reactjs/src/App.jsx
@@ -4,7 +4,7 @@ import { Post } from "./Post";
 import styles from './App.module.css';
 
 import './global.css';
-import {Sidebar} from './components/Sidebar.jsx';
+import {Sidebar} from './components/Sidebar';
 
 export function App() {
   return (
