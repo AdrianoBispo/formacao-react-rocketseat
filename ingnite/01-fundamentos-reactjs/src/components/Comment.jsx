91ebe427f2adcef2d6d3cead1fe8c5c91d41aa56
Adriano Bispo <drianvini10@gmail.com>
2025-03-21 17:57:10 -0300

    Estrutura da aplicação - Componente: Avatar

--- ingnite/01-fundamentos-reactjs/src/components/Comment.jsx
+++ ingnite/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,20 +1,19 @@
 import styles from "./Comment.module.css";
 
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/diego3g.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
               <strong>Diego Fernandes</strong>
-              <time title="11 de Maio às 08:13" datetime="2022-05-11 08:13:00">
-                Cerca de 1h atrás
-              </time>
+              <time title="11 de Maio às 08:13" datetime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
 
             <button title="Deletar comentário">
