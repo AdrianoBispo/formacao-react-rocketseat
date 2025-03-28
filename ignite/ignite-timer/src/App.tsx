33bfba8c9096f92cc8a7506515f281e47c7e4715
Adriano Bispo <drianvini10@gmail.com>
2025-03-28 12:20:13 -0300

    Estrutura da aplicação - Styled Components

--- ignite/ignite-timer/src/App.tsx
+++ ignite/ignite-timer/src/App.tsx
@@ -3,10 +3,10 @@ import { Button } from "./components/Button";
 export function App() {
   return (
     <>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="sucess"/>
+      <Button variant="danger"/>
     </>
   );
 }
