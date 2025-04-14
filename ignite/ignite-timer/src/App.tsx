57d0cd9230ee4ae4d297f764ecf54d7ea4579d04
Adriano Bispo <drianvini10@gmail.com>
2025-04-14 15:39:43 -0300

    Páginas e rotas - React Router DOM

--- ignite/ignite-timer/src/App.tsx
+++ ignite/ignite-timer/src/App.tsx
@@ -1,19 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="sucess"/>
-      <Button variant="danger"/>
-      <Button />
-
-      <GlobalStyle /> 
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
