130738ac2df6344411b474d46c36fbb06778a980
Adriano Bispo <drianvini10@gmail.com>
2025-04-16 15:29:09 -0300

    Funcionalidades da aplicação - Separando componentes

--- ignite/ignite-timer/src/pages/Home/index.tsx
+++ ignite/ignite-timer/src/pages/Home/index.tsx
@@ -15,6 +15,8 @@ import {
   StopCountdownButton,
   TaskInput,
 } from "./styles";
+import { NewCycleForm } from "./components/NewCycleForm";
+import { Countdown } from "./components/Countdown";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
@@ -61,6 +63,7 @@ export function Home() {
           new Date(),
           activeCycle.startDate
         );
+
         if (secondsDifference >= totalSeconds) {
           setCycles((state) =>
             state.map((cycle) => {
@@ -103,7 +106,7 @@ export function Home() {
   }
 
   function handleInterruptCycle() {
-    setCycles((state) => 
+    setCycles((state) =>
       state.map((cycle) => {
         if (cycle.id === activeCycleId) {
           return { ...cycle, interruptedDate: new Date() };
@@ -132,50 +135,11 @@ export function Home() {
   const task = watch("task");
   const isSubmitDisable = !task;
 
-  console.log(cycles);
-
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)}>
-        <FormContainer>
-          <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput
-            id="task"
-            list="task-suggestions"
-            placeholder="Dê um nome para o seu projeto"
-            disabled={!!activeCycle}
-            {...register("task")}
-          />
-
-          <datalist id="task-suggestions">
-            <option value="Projeto 1" />
-            <option value="Projeto 2" />
-            <option value="Projeto 3" />
-            <option value="Banana" />
-          </datalist>
-
-          <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput
-            type="number"
-            id="minutesAmount"
-            placeholder="00"
-            step={5}
-            min={5}
-            max={60}
-            disabled={!!activeCycle}
-            {...register("minutesAmount", { valueAsNumber: true })}
-          />
-
-          <span>minutos.</span>
-        </FormContainer>
-
-        <CountdownContainer>
-          <span>{minutes[0]}</span>
-          <span>{minutes[1]}</span>
-          <Separator>:</Separator>
-          <span>{seconds[0]}</span>
-          <span>{seconds[1]}</span>
-        </CountdownContainer>
+        <NewCycleForm />
+        <Countdown />
 
         {activeCycle ? (
           <StopCountdownButton onClick={handleInterruptCycle} type="button">
